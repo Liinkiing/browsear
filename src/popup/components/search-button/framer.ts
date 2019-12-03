150089a9e315bcb4abac97f5f13688bd1b24a1ad
Omar Jbara <omar.jbara2@gmail.com>
2019-12-03 23:40:42 +0100

    - Fixed record button transition animation

--- src/popup/components/search-button/framer.ts
+++ src/popup/components/search-button/framer.ts
@@ -3,13 +3,17 @@ import { Variants } from 'framer-motion'
 export const variants = {
   SearchButtonInner: {
     initial: {
+      transition: {
+        ease: 'circOut',
+        duration: 0.1
+      },
       borderRadius: '50%',
       width: 300,
       height: 300
     },
     recording: {
       transition: {
-        ease: 'easeIn',
+        ease: 'circIn',
         duration: 0.1
       },
       borderRadius: '0%',
